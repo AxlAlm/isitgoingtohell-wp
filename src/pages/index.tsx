e9e497b2b89924d90926125c83724035de518dca
Axel Almquist <axel@almquist.io>
2023-01-06 14:10:19 +0100

    adding sliders for time ranges

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,9 @@ import {
   Sphere,
   Graticule,
 } from "react-simple-maps";
+import Slider from "@mui/material/Slider";
+import Box from "@mui/material/Box";
+
 import { Region } from "../server/api/routers/regions";
 
 const geoUrl = "/features.json";
@@ -24,59 +27,77 @@ const colorScale = scaleLinear([0, 1], ["white", "red"]);
 const start = new Date("2010-01-01");
 const end = new Date("2024-01-01");
 
-const Home: NextPage = () => {
-  const [data, setData] = useState<Region[]>([]);
+function valuetext(value: number) {
+  return `${value}°C`;
+}
 
-  // const regions = api.regions.all.useQuery({
-  //   to_date: end,
-  //   from_date: start,
-  // });
-  // console.log("HOELELEO", regions.data);
+const Home: NextPage = () => {
+  const regions = api.regions.all.useQuery({
+    to_date: end,
+    from_date: start,
+  });
+
+  const [data, setData] = useState<Region[]>(regions.data || []);
+  const [value, setValue] = useState<number[]>([20, 37]);
+
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    setValue(newValue as number[]);
+  };
 
   useEffect(() => {
-    setData(() => {
-      const regions = api.regions.all.useQuery({
-        to_date: end,
-        from_date: start,
-      });
-      setData(regions.data as Region[]);
-    });
-    // fetchData();
-  }, []);
+    setData(regions.data || []);
+  }, [data]);
 
   console.log("DATA", data);
 
   return (
-    <ComposableMap
-      projectionConfig={{
-        rotate: [-10, 0, 0],
-        scale: 147,
-      }}
+    <Box
+      sx={{ width: 1200 }}
+      display="flex"
+      alignItems="center"
+      flexDirection="column"
     >
-      <Sphere id="1" fill="white" stroke="#E4E5E6" strokeWidth={0.5} />
-      <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
-      {(data || []).length > 0 && (
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map((geo) => {
-              const d = data.find(
-                (x) =>
-                  x.region === (iso3ToRegion.get(geo.id) || "").toLowerCase()
-              );
-              console.log(iso3ToRegion);
-              console.log("FOKEOFKEOFKOEF", data);
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill={d ? colorScale(d.score) : "#F5F4F6"}
-                />
-              );
-            })
-          }
-        </Geographies>
-      )}
-    </ComposableMap>
+      <Box sx={{ width: 300 }}>
+        <Slider
+          getAriaLabel={() => "Temperature range"}
+          value={value}
+          onChange={handleChange}
+          valueLabelDisplay="auto"
+          getAriaValueText={valuetext}
+          color="primary"
+          // colorPrimary="red"
+        />
+      </Box>
+
+      <ComposableMap
+        projectionConfig={{
+          rotate: [-10, 0, 0],
+          scale: 147,
+        }}
+      >
+        <Sphere id="1" fill="white" stroke="#E4E5E6" strokeWidth={0.5} />
+        <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
+        {(data || []).length > 0 && (
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map((geo) => {
+                const d = data.find(
+                  (x) =>
+                    x.region === (iso3ToRegion.get(geo.id) || "").toLowerCase()
+                );
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill={d ? colorScale(d.score) : "#F5F4F6"}
+                  />
+                );
+              })
+            }
+          </Geographies>
+        )}
+      </ComposableMap>
+    </Box>
   );
 };
 
