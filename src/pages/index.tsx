63958e199b6289f86fea036cf2c76f29eb3a003c
Axel Almquist <axel@almquist.io>
2023-01-08 01:58:08 +0100

    finished range picker and map, added fonts and coloring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,14 @@ import {
   Sphere,
   Graticule,
 } from "react-simple-maps";
-import Slider from "@mui/material/Slider";
 import Box from "@mui/material/Box";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 import { Region } from "../server/api/routers/regions";
+import dayjs, { Dayjs } from "dayjs";
+import TextField from "@mui/material/TextField";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 const geoUrl = "/features.json";
 
@@ -24,79 +28,135 @@ const iso3ToRegion: Map<string, string> = new Map(
 
 const colorScale = scaleLinear([0, 1], ["white", "red"]);
 
-const start = new Date("2010-01-01");
-const end = new Date("2024-01-01");
-
-function valuetext(value: number) {
-  return `${value}°C`;
-}
+// const useStyles = makeStyles({
+//   paper: {
+//     fontFamily: "VCR_OSD_MONO",
+//   },
+// });
 
 const Home: NextPage = () => {
+  const min = dayjs("2020-01-01");
+  const max = dayjs().add(1, "month");
+
+  const [data, setData] = useState<Region[]>([]);
+  const [startMonth, setStartMonth] = useState<Dayjs>(min);
+  const [endMonth, setEndMonth] = useState<Dayjs>(max);
+
   const regions = api.regions.all.useQuery({
-    to_date: end,
-    from_date: start,
+    to_date: endMonth.toDate(),
+    from_date: startMonth.toDate(),
   });
 
-  const [data, setData] = useState<Region[]>(regions.data || []);
-  const [value, setValue] = useState<number[]>([20, 37]);
+  const handleStartMonthChange = (newValue: Dayjs | null) => {
+    if (newValue === null) {
+      return;
+    }
+    setStartMonth(newValue);
+  };
 
-  const handleChange = (event: Event, newValue: number | number[]) => {
-    setValue(newValue as number[]);
+  const handleEndMonthChange = (newValue: Dayjs | null) => {
+    if (newValue === null) {
+      return;
+    }
+
+    setEndMonth(newValue);
   };
 
   useEffect(() => {
     setData(regions.data || []);
-  }, [data]);
-
-  console.log("DATA", data);
+  }, [data, startMonth, endMonth]);
 
   return (
     <Box
-      sx={{ width: 1200 }}
       display="flex"
       alignItems="center"
       flexDirection="column"
+      justifyContent="center"
+      minHeight="120vh"
+      bgcolor="#ff71ce"
     >
-      <Box sx={{ width: 300 }}>
-        <Slider
-          getAriaLabel={() => "Temperature range"}
-          value={value}
-          onChange={handleChange}
-          valueLabelDisplay="auto"
-          getAriaValueText={valuetext}
-          color="primary"
-          // colorPrimary="red"
-        />
-      </Box>
-
-      <ComposableMap
-        projectionConfig={{
-          rotate: [-10, 0, 0],
-          scale: 147,
-        }}
+      <div className="my-div"> Is it Going to hell?</div>
+      <Box
+        display="flex"
+        alignItems="center"
+        flexDirection="row"
+        sx={{ p: 2, border: "1px dashed grey" }}
       >
-        <Sphere id="1" fill="white" stroke="#E4E5E6" strokeWidth={0.5} />
-        <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
-        {(data || []).length > 0 && (
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map((geo) => {
-                const d = data.find(
-                  (x) =>
-                    x.region === (iso3ToRegion.get(geo.id) || "").toLowerCase()
-                );
-                return (
-                  <Geography
-                    key={geo.rsmKey}
-                    geography={geo}
-                    fill={d ? colorScale(d.score) : "#F5F4F6"}
-                  />
-                );
-              })
-            }
-          </Geographies>
-        )}
-      </ComposableMap>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            InputProps={{
+              style: {
+                fontFamily: "VCR_OSD_MONO",
+                fontWeight: "bold",
+                color: "#fffb96",
+              },
+            }}
+            PopperProps={{
+              style: {
+                fontFamily: "VCR_OSD_MONO",
+              },
+            }}
+            views={["year", "month"]}
+            minDate={min}
+            maxDate={max}
+            value={startMonth}
+            onChange={handleStartMonthChange}
+            renderInput={(params) => (
+              <TextField {...params} helperText={null} />
+            )}
+          />
+          <DatePicker
+            InputProps={{
+              style: {
+                fontFamily: "VCR_OSD_MONO",
+                fontWeight: "bold",
+                color: "#fffb96",
+              },
+            }}
+            views={["year", "month"]}
+            minDate={min}
+            maxDate={max}
+            value={endMonth}
+            onChange={handleEndMonthChange}
+            renderInput={(params) => (
+              <TextField {...params} helperText={null} />
+            )}
+          />
+        </LocalizationProvider>
+      </Box>
+      <Box sx={{ width: 1000, p: 2, border: "1px dashed grey" }}>
+        <div style={{ pointerEvents: "none" }}>
+          <ComposableMap
+            projectionConfig={{
+              rotate: [-10, 0, 0],
+              scale: 140,
+            }}
+          >
+            <Sphere id="1" fill="#b967ff" stroke="#05ffa1" strokeWidth={0.7} />
+            <Graticule stroke="#05ffa1" strokeWidth={0.7} />
+            {(data || []).length > 0 && (
+              <Geographies geography={geoUrl}>
+                {({ geographies }) =>
+                  geographies.map((geo) => {
+                    const d = data.find(
+                      (x) =>
+                        x.region ===
+                        (iso3ToRegion.get(geo.id) || "").toLowerCase()
+                    );
+                    return (
+                      <Geography
+                        key={geo.rsmKey}
+                        geography={geo}
+                        fill={d ? colorScale(d.score) : "#F5F4F6"}
+                      />
+                    );
+                  })
+                }
+              </Geographies>
+            )}
+          </ComposableMap>{" "}
+        </div>
+      </Box>
     </Box>
   );
 };
